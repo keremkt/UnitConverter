575be259a54d144354f84c9e54252a0c4b60e429
keremkrtras <keremkrtas@gmail.com>
2023-07-09 21:21:40 +0300

    First Commit

--- app/src/main/java/com/biiapps/unitconverter/util/Utils.kt
+++ app/src/main/java/com/biiapps/unitconverter/util/Utils.kt
@@ -2,49 +2,42 @@ package com.biiapps.unitconverter.util
 
 
 fun Int.metSecondToKmh(i: Double): Double {
-    val r =3.6*i
-    return r
+    return 3.6 * i
 }
 
 fun Int.milesHourToKmh(i: Double): Double {
-    val r = i*1609344/1000000
 
-    return r
+    return i * 1609344 / 1000000
 }
 
 fun Int.milesSecondToKmh(i: Double): Double {
-    val r = i*57936384/10000
 
-    return r
+    return i * 57936384 / 10000
 }
 
 fun Int.feetHourToKmh(i: Double): Double {
-    val r = i*3/10000
 
-    return r
+    return i * 3 / 10000
 }
 
 fun Int.feetMinToKmh(i: Double): Double {
-    val r = i*18288/1000000
 
-    return r
+    return i * 18288 / 1000000
 }
 
 fun Int.feetSecondToKmh(i: Double): Double {
-    val r = i*109728/100000
 
-    return r
+    return i * 109728 / 100000
 }
+
 //Todo:
 fun Int.kmhToMetSecond(i: Double): Double {
-    val r =i/3.6
-    return r
+    return i / 3.6
 }
 
 fun Int.kmhToMilesHour(i: Double): Double {
-    val r = i*1000000/1609344
 
-    return r
+    return i * 1000000 / 1609344
 }
 
 fun Int.kmhToMilesSecond(i: Double): Double {
@@ -54,18 +47,15 @@ fun Int.kmhToMilesSecond(i: Double): Double {
 }
 
 fun Int.kmhToFeetHour(i: Double): Double {
-    val r = i*10000/3
 
-    return r
+    return i * 10000 / 3
 }
 
 fun Int.kmhToFeetMin(i: Double): Double {
-    val r = i*1000000/18288
 
-    return r
+    return i * 1000000 / 18288
 }
 
 fun Int.kmhToFeetSecond(i: Double): Double {
-    val r = i*100000/109728
-    return r
+    return i * 100000 / 109728
 }
